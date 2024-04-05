911662e6b95f2f495e33d868d50632d6a6371e2e
kacpers27325 <s27325@pjwstk.edu.pl>
2024-04-05 12:58:57 +0200

    z

---
+++ REST API/REST API/Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace REST_API.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string Category { get; set; }
+    public double Mass { get; set; }
+    public string FurColor { get; set; }
+}
